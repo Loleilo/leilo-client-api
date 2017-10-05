2765bc5181c69309f20c36b1c5a0939ff02721e3
Sunny <sunny.lan.coder@gmail.com>
2017-10-05 13:04:25 -0400

    Preparing for first release

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
-require('leilo-backend')(); //run server
+require('leilo-backend')({persist: false}); //run server
 const client = require('../index');
 const config = require('../config');
 const localID = config.localID;
-const serverID=config.serverID;
+const serverID = config.serverID;
 
 const conn = client('http://127.0.0.1:80', {
     username: "leilo",
